1f83582c28d32579d8795adda1bc03d044ec5470
kei-s16 <kei1216s@nifty.com>
2023-04-15 10:37:20 +0900

    honoのドキュメントに素直に従うことで解消

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import { Hono } from 'hono';
 import type { Database } from '@cloudflare/d1';
 
-export interface Env {
-  DB: Database;
+type Bindings = {
+  DB: Database // Cloudflare D1
 }
 
-// TODO: Envに型エラーが出るので直す
-const app = new Hono<Env>();
+const app = new Hono<{Bindings: Bindings}>();
 
 app.get('/', () => {
   return new Response(
@@ -14,7 +13,6 @@ app.get('/', () => {
   );
 });
 
-// TODO: contextに型エラーが出るので直す
 app.get('/api/tables/show', async (context) => {
   const { results } = await context.env.DB.prepare(
     "select name from sqlite_master where type='table'"
